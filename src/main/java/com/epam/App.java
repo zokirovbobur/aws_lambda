57df3064498a0d059ed1f067a5e6e43c6335c8a2
Bobur_Zokirov <zokirovbobur93@gmail.com>
2021-10-11 13:15:41 +0500

    transfer code from WelcomeLambda.java to App.java

--- src/main/java/com/epam/App.java
+++ src/main/java/com/epam/App.java
@@ -3,15 +3,38 @@ package com.epam;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import java.util.LinkedHashMap;
+
 public class App implements RequestHandler<Object, Object>
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	private DynamoConnector connector = new DynamoConnector();
 
 	@Override
-	public Object handleRequest(Object o, Context context) {
-		return null;
+	public Object handleRequest(Object input, Context context) {
+		System.out.println("Hello world");
+		return "Hello world";
+	}
+
+	public Object insertProduct(Object input, Context context) {
+		System.out.println(input);
+		System.out.println(context.getAwsRequestId());
+		Product product = fromInput(input);
+		return connector.insertProduct(product);
+	}
+
+	public Object updateProduct(Object input, Context context) {
+		System.out.println(input);
+		System.out.println(context.getAwsRequestId());
+		Product product = fromInput(input);
+		return connector.updateProduct(product);
+	}
+
+	public Product fromInput(Object input){
+		LinkedHashMap<String, String> map = (LinkedHashMap)input;
+		System.out.println(map);
+		String id = map.get("id");
+		Product product = new Product(id,id);
+		System.out.println(product);
+		return product;
 	}
 }
